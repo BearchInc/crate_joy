14b8dd3cca7d7e30ef15917fcc4e4c274c49f65f
Daniel <daniel.amarante2@gmail.com>
2016-03-15 16:16:33 -0300

    Extract shipments path.

--- lib/crate_joy.rb
+++ lib/crate_joy.rb
@@ -3,34 +3,46 @@ require 'crate_joy/order.rb'
 require 'crate_joy/version'
 
 module CrateJoy
-
-  class API
-
-    def self.client(id, key)
-      Client.new(id, key)
+    
+    class API
+        
+        def self.client(id, key)
+        Client.new(id, key)
     end
-
+    
     class Client
-      def initialize(id, key)
-        @id = id
-        @key = key
-        @resource = ::RestClient::Resource.new "https://api.cratejoy.com/v1", id, key
-      end
-
-      def orders date
-        resource = @resource["/shipments/?shipped_at__ge=#{date}"]
-        response = JSON.parse resource.get
-        if response["results"]
-          results = response["results"]
-          results = results.map do |result|
-            Order.new(result)
-          end
-        else
-          results = []
+        
+        BASE_URL = 'https://api.cratejoy.com/v1'
+        
+        def initialize(id, key)
+            @resource = ::RestClient::Resource.new BASE_URL, id, key
         end
-        results
-      end
+        
+        def orders(date)
+            resource = orders_path(date)
+            response = JSON.parse resource.get
+            if response['results']
+                results = response['results']
+                results = results.map do |result|
+                    Order.new(result)
+                end
+                else
+                results = []
+            end
+            results
+        end
+        
+        private
+        def orders_path(date)
+            date = format_date(date)
+            @resource["/shipments/?shipped_at__ge=#{date}"]
+        end
+        
+        def format_date(date)
+            date.strftime('%FT%TZ')
+        end
+        
     end
-
-  end
+    
+end
 end
