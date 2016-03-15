8bfa60eb4170e02cebe0b8bbbce499b8b0b475eb
Daniel <daniel.amarante2@gmail.com>
2016-03-15 16:28:57 -0300

    Extract client class.

--- lib/crate_joy.rb
+++ lib/crate_joy.rb
@@ -1,5 +1,4 @@
-require 'rest-client'
-require 'crate_joy/order.rb'
+require 'crate_joy/client.rb'
 require 'crate_joy/version'
 
 module CrateJoy
@@ -8,40 +7,6 @@ module CrateJoy
 
     def self.client(id, key)
       Client.new(id, key)
-    end
-
-    class Client
-
-      BASE_URL = 'https://api.cratejoy.com/v1'
-
-      def initialize(id, key)
-        @resource = ::RestClient::Resource.new BASE_URL, id, key
-      end
-
-      def orders(date)
-        resource = orders_path(date)
-        response = JSON.parse resource.get
-        if response['results']
-          results = response['results']
-          results = results.map do |result|
-            Order.new(result)
-          end
-        else
-          results = []
-        end
-        results
-      end
-
-      private
-      def orders_path(date)
-        date = format_date(date)
-        @resource["/shipments/?shipped_at__ge=#{date}"]
-      end
-
-      def format_date(date)
-        date.strftime('%FT%TZ')
-      end
-
     end
 
   end
---
+++ lib/crate_joy/client.rb
@@ -0,0 +1,36 @@
+require 'rest-client'
+require 'crate_joy/order.rb'
+
+class Client
+
+  BASE_URL = 'https://api.cratejoy.com/v1'
+
+  def initialize(id, key)
+    @resource = ::RestClient::Resource.new BASE_URL, id, key
+  end
+
+  def orders(date)
+    resource = orders_path(date)
+    response = JSON.parse resource.get
+    if response['results']
+      results = response['results']
+      results = results.map do |result|
+        Order.new(result)
+      end
+    else
+      results = []
+    end
+    results
+  end
+
+  private
+  def orders_path(date)
+    date = format_date(date)
+    @resource["/shipments/?shipped_at__ge=#{date}"]
+  end
+
+  def format_date(date)
+    date.strftime('%FT%TZ')
+  end
+
+end
