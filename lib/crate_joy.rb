f734c7d90f6da89b19112fcb2fc50c0592474997
Daniel <daniel.amarante2@gmail.com>
2016-03-15 15:20:21 -0300

    Create client class.

--- lib/crate_joy.rb
+++ lib/crate_joy.rb
@@ -3,29 +3,34 @@ require 'crate_joy/order.rb'
 require 'crate_joy/version'
 
 module CrateJoy
-  def self.orders
-    API.orders
-  end
 
   class API
 
     def self.client(id, key)
-      ::RestClient::Resource.new "https://api.cratejoy.com/v1", id, key
+      Client.new(id, key)
     end
 
-    def self.orders
-      client = CrateJoy::API.client(ENV["ACCOUNT"], ENV["PASSWORD"])
-      resource = client["/shipments/?shipped_at__ge=2016-02-15T00:00:00Z"]
-      response = JSON.parse resource.get
-      if response["results"]
-        results = response["results"]
-        results = results.map do |result|
-          Order.new(result)
-        end
-      else
-        results = []
+    class Client
+      def initialize(id, key)
+        @id = id
+        @key = key
+        @resource = ::RestClient::Resource.new "https://api.cratejoy.com/v1", id, key
       end
-      results
+
+      def orders date
+        resource = @resource["/shipments/?shipped_at__ge=#{date}"]
+        response = JSON.parse resource.get
+        if response["results"]
+          results = response["results"]
+          results = results.map do |result|
+            Order.new(result)
+          end
+        else
+          results = []
+        end
+        results
+      end
     end
+
   end
 end
