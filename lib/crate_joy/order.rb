1420290de452ce10d24e6d87eb0bb64bcaec6dd1
Daniel <daniel.amarante2@gmail.com>
2016-03-16 14:06:27 -0300

    Add all variables fetched from order request.

--- lib/crate_joy/order.rb
+++ lib/crate_joy/order.rb
@@ -1,12 +1,43 @@
 require 'crate_joy/box.rb'
 
 class Order
-  attr_accessor :name, :customer_id, :tracking_number
+  attr_accessor :adjusted_ordered_at
+  attr_accessor :created_at
+  attr_accessor :customer
+  attr_accessor :customer_id
+  attr_accessor :fulfillments
+  attr_accessor :id
+  attr_accessor :is_gift
+  attr_accessor :is_test
+  attr_accessor :labels
+  attr_accessor :ship_address
+  attr_accessor :shipped_at
+  attr_accessor :status
+  attr_accessor :target_at
+  attr_accessor :tracking_number
+  attr_accessor :type
+  attr_accessor :url
+
   def initialize(json={})
-    self.tracking_number = json['tracking_number']
-    self.name = json['customer']['name']
-    self.customer_id = json['customer_id']
+    @id = json['id']
+    @adjusted_ordered_at = json['adjusted_ordered_at']
+    @created_at = json['created_at']
+    @is_test = json['is_test']
+    @shipped_at = json['shipped_at']
+    @status = json['status']
+    @target_at = json['target_at']
+    @tracking_number = json['tracking_number']
+    @type = json['type']
+    @url = json['url']
+    @is_gift = json['is_gift']
+
+    @customer = json['customer']
+    @fulfillments = json['fulfillments']
+    @labels = json['labels']
+    @customer_id = json['customer_id']
+    @ship_address = json['ship_address']
   end
+
   def self.boxes
     [
         Box.new('Month 3', 1),
@@ -20,5 +51,9 @@ class Order
         Box.new('Postpartum 2', 9),
         Box.new('Postpartum 3', 10)
     ]
+  end
+
+  def self.name
+    @customer['name']
   end
 end
