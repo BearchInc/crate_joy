6835703df1d4dd3d63d7395692b26f231d8f0bb5
Daniel <daniel.amarante2@gmail.com>
2016-03-17 11:20:46 -0300

    Add fulfillment class.

---
+++ lib/crate_joy/fulfillment.rb
@@ -0,0 +1,36 @@
+class Fulfillment
+  attr_accessor :adjusted_fulfillment_date
+  attr_accessor :cycle_number
+  attr_accessor :fulfillment_date
+  attr_accessor :id
+  attr_accessor :instance
+  attr_accessor :is_test
+  attr_accessor :order
+  attr_accessor :shipment_id
+  attr_accessor :shipping
+  attr_accessor :status
+  attr_accessor :subscription_id
+  attr_accessor :tax
+  attr_accessor :test
+  attr_accessor :total_cycles
+  attr_accessor :type
+
+  def initialize(json={})
+    @adjusted_fulfillment_date = json['adjusted_fulfillment_date']
+    @cycle_number = json['cycle_number']
+    @fulfillment_date = json['fulfillment_date']
+    @id = json['id']
+    @instance = json['instance']
+    @is_test = json['is_test']
+    @order = json['order']
+    @shipment_id = json['shipment_id']
+    @shipping = json['shipping']
+    @status = json['status']
+    @subscription_id = json['subscription_id']
+    @tax = json['tax']
+    @test = json['test']
+    @total_cycles = json['total_cycles']
+    @type = json['type']
+  end
+
+end
